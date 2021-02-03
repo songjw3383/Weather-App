0c80144b251e026fb27c039bafec8e333943fe98
songjw3383 <56250064+songjw3383@users.noreply.github.com>
2021-02-04 01:07:35 +0900

    0204

--- App.js
+++ App.js
@@ -1,21 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Alert} from "react-native";
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello!!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+  state = {
+    isLoading : true
+  };
+  getLocation = async() => {
+    try {
+      await Location.requestPermissionsAsync();// 위치정보의 허용여부
+      const { coords: { latitude, longitude}} = await Location.getCurrentPositionAsync(); // 위치정보를 받아오기 위함.
+      this.setState({ isLoading : false });
+
+    } catch(error) {
+      Alert.alert("Can't find you", "So sad"); //에러 메세지
+    }
+  }
+  componentDidMount() {
+    this.getLocation();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return isLoading ? <Loading /> : null;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
